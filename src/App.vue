837be38bbe49f16e9380eeeb133bf25fee260262
Monopich <126034376+Monopich@users.noreply.github.com>
2023-10-20 08:18:32 +0700

    Finish task1: - Creat my first vuejs application.

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Category from './components/Category.vue';
+
+</script>
+
+<template>
+  <h1>This is my first Veujs project.</h1>
+
+</template>
+
+<style scoped>
+
+</style>
