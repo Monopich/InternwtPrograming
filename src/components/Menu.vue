76db726db77165ac5841a88b057ee1d28ab56665
Monopich <126034376+Monopich@users.noreply.github.com>
2023-11-16 19:41:31 +0700

    add tp02

---
+++ src/components/Menu.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div class="Feature">
+            <h2>Feature Categories</h2>
+        </div>
+        <div class="Menu">
+
+        </div>
+    </div>
+</template>
+
+<style>
+    .container{
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
